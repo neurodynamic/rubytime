852717db63fd4e5f34386e2819c1b622e969a4b0
neurodynamic <neurodynamicdev@gmail.com>
2015-08-31 07:50:59 -0400

    improve examples, randomly generate date/time

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -2,12 +2,14 @@
 module Main
   class MainController < Volt::ModelController
     def index
-      page._selected_date = Time.now.strftime('%F')
-      page._selected_time = Time.now.strftime('%R')
-      page._time_string_1 = '%R on %D'
-      page._time_string_2 = '%l:%M%P %A, %B %-m, %Y'
-      page._time_string_3 = 'Day %j, week %V of %Y'
-      page._time_string_4 = '%s seconds past Unix Epoch'
+      page._selected_date = "#{Time.now.strftime('%Y')}-0#{(1..9).to_a.sample}-0#{(1..9).to_a.sample}"
+      page._selected_time = "#{(13..23).to_a.sample}:#{(10..59).to_a.sample}"
+
+      page._time_string_1 = '%l:%M%P %A, %B %-m, %Y'
+      page._time_string_2 = '%Y, %C, %y'
+      page._time_string_3 = '%B, %b, %m, %-m'
+      page._time_string_4 = '%A, %a, %u, %w; %d, %-d'
+      page._time_string_5 = '%H, %k, %I, %l:%M%P'
       # Add code for when the index view is loaded
     end
 
